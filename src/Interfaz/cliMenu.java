b9a5b7b305efe5b0bf4731a8ccd6b65dd70cc6d8
casc3798 <casc3798@gmail.com>
2017-05-27 13:41:17 -0500

    Menu completo

--- src/Interfaz/cliMenu.java
+++ src/Interfaz/cliMenu.java
@@ -52,6 +52,7 @@ public class cliMenu extends javax.swing.JFrame {
                 modelo.addRow(fila);
             }
             menu.setModel(modelo);
+            con.close();
         } catch (SQLException ex) {
             Logger.getLogger(cliMenu.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -64,8 +65,12 @@ public class cliMenu extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         menu = new javax.swing.JTable();
+        consultar = new javax.swing.JButton();
+        sede = new java.awt.TextField();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Menu");
         setBackground(new java.awt.Color(51, 51, 51));
 
         jPanel1.setBackground(new java.awt.Color(0, 0, 0));
@@ -89,6 +94,25 @@ public class cliMenu extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(menu);
 
+        consultar.setText("Consultar");
+        consultar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                consultarActionPerformed(evt);
+            }
+        });
+
+        sede.setForeground(new java.awt.Color(102, 102, 255));
+        sede.setText("Todas");
+        sede.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sedeActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setFont(new java.awt.Font("Lucida Calligraphy", 0, 14)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Sede:");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -100,7 +124,14 @@ public class cliMenu extends javax.swing.JFrame {
                         .addComponent(jLabel2))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(119, 119, 119)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 453, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 453, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(187, 187, 187)
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(sede, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(108, 108, 108)
+                        .addComponent(consultar)))
                 .addContainerGap(168, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -110,7 +141,12 @@ public class cliMenu extends javax.swing.JFrame {
                 .addComponent(jLabel2)
                 .addGap(38, 38, 38)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 286, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(71, Short.MAX_VALUE))
+                .addGap(11, 11, 11)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(sede, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(consultar)
+                    .addComponent(jLabel3))
+                .addContainerGap(35, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -126,6 +162,42 @@ public class cliMenu extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void consultarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_consultarActionPerformed
+        try {
+            Connection con = null;
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(cliMenu.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/entregafinalbd", "root", "ragnarok3798");
+            Statement st = con.createStatement();
+            String sql = "Select * from menu where (Sede = " + sede.getText() +") " ;
+            ResultSet rstb = st.executeQuery(sql);
+            ResultSetMetaData rsmd = rstb.getMetaData();
+            int col = rsmd.getColumnCount();
+            DefaultTableModel modelo = new DefaultTableModel();
+            for (int i = 1; i <= col; i++) {
+                modelo.addColumn(rsmd.getColumnLabel(i));
+            }
+            while (rstb.next()){
+                String fila[] = new String [col];
+                for (int j = 0; j < col; j++) {
+                    fila[j] = rstb.getString(j+1);
+                }
+                modelo.addRow(fila);
+            }
+            menu.setModel(modelo);
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(cliMenu.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_consultarActionPerformed
+
+    private void sedeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sedeActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_sedeActionPerformed
 
     /**
      * @param args the command line arguments
@@ -165,9 +237,12 @@ public class cliMenu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton consultar;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable menu;
+    private java.awt.TextField sede;
     // End of variables declaration//GEN-END:variables
 }
