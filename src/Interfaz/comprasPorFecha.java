6749323102ea8c9a1d2027937bed7a2d7216057f
DESKTOP-J2R8B4R\dcast <danicastel95@hotmail.com>
2017-05-28 01:35:26 -0500

    version daniela

--- src/Interfaz/comprasPorFecha.java
+++ src/Interfaz/comprasPorFecha.java
@@ -25,24 +25,28 @@ public class comprasPorFecha extends javax.swing.JFrame {
         initComponents();
         setLocationRelativeTo(null); 
         setResizable(false);
-        setTitle("Login");
+        setTitle("Compras por fecha");
+        
+        ImageIcon imagen = new ImageIcon(getClass().getResource("/Imagenes/BotonCerrarAzul.png"));
+        Icon icono= new ImageIcon(imagen.getImage().getScaledInstance(30,30,20));
+        jBotonVolver.setIcon(icono);
     }
     
     public Integer llamar(String date){
         int numero = 0;
         try {
-            Connection con = null;
+         
             try {
                 Class.forName("com.mysql.jdbc.Driver");
             } catch (ClassNotFoundException ex) {
                 Logger.getLogger(comprasPorFecha.class.getName()).log(Level.SEVERE, null, ex);
             }
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/entregafinalbd", "root", "ragnarok3798");
-            PreparedStatement st = con.prepareStatement("select contarCompraPorFecha("+date+")");
+       
+            PreparedStatement st = MySQL.conn.prepareStatement("select contarCompraPorFecha("+date+")");
             ResultSet resultado = st.executeQuery();
             resultado.next();
             numero = resultado.getInt(1);
-            con.close();
+
         } catch (SQLException ex) {
             Logger.getLogger(comprasPorFecha.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -59,21 +63,21 @@ public class comprasPorFecha extends javax.swing.JFrame {
         boton = new javax.swing.JButton();
         resultado = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        jBotonVolver = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Menu");
         setBackground(new java.awt.Color(51, 51, 51));
 
-        jPanel1.setBackground(new java.awt.Color(0, 0, 0));
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setForeground(new java.awt.Color(255, 255, 255));
         jPanel1.setPreferredSize(new java.awt.Dimension(720, 400));
 
-        jLabel2.setFont(new java.awt.Font("Lucida Calligraphy", 0, 24)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setFont(new java.awt.Font("Copperplate Gothic Light", 1, 24)); // NOI18N
         jLabel2.setText("Numero de compras de insumos por fecha");
 
         fecha.setFont(new java.awt.Font("Lucida Calligraphy", 0, 18)); // NOI18N
-        fecha.setForeground(new java.awt.Color(255, 255, 255));
         fecha.setText("Fecha (YYYY-MM-DD):");
 
         inputFecha.setFont(new java.awt.Font("Lucida Calligraphy", 0, 14)); // NOI18N
@@ -84,6 +88,9 @@ public class comprasPorFecha extends javax.swing.JFrame {
             }
         });
 
+        boton.setBackground(new java.awt.Color(0, 102, 102));
+        boton.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 14)); // NOI18N
+        boton.setForeground(new java.awt.Color(255, 255, 255));
         boton.setText("Consultar");
         boton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -91,13 +98,29 @@ public class comprasPorFecha extends javax.swing.JFrame {
             }
         });
 
-        resultado.setFont(new java.awt.Font("Lucida Calligraphy", 0, 14)); // NOI18N
-        resultado.setForeground(new java.awt.Color(255, 255, 255));
-        resultado.setText("Resultado");
+        resultado.setFont(new java.awt.Font("Lucida Calligraphy", 0, 36)); // NOI18N
+        resultado.setText("#");
 
         jLabel4.setFont(new java.awt.Font("Lucida Calligraphy", 0, 18)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("Resultado:");
+
+        jBotonVolver.setBackground(new java.awt.Color(153, 0, 0));
+        jBotonVolver.setBorder(null);
+        jBotonVolver.setBorderPainted(false);
+        jBotonVolver.setContentAreaFilled(false);
+        jBotonVolver.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jBotonVolver.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jBotonVolver.setIconTextGap(6);
+        jBotonVolver.setMargin(new java.awt.Insets(0, 0, 0, 0));
+        jBotonVolver.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBotonVolverActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Microsoft YaHei UI", 0, 11)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 102, 102));
+        jLabel1.setText("Volver");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -114,19 +137,30 @@ public class comprasPorFecha extends javax.swing.JFrame {
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(resultado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(inputFecha, javax.swing.GroupLayout.DEFAULT_SIZE, 175, Short.MAX_VALUE))
-                        .addGap(44, 44, 44)
+                        .addGap(34, 34, 34)
                         .addComponent(boton))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(91, 91, 91)
+                        .addGap(19, 19, 19)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jBotonVolver, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(89, 89, 89)
                         .addComponent(jLabel2)))
                 .addContainerGap(76, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(19, 19, 19)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel1)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jBotonVolver, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(16, 16, 16)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel2)
-                .addGap(124, 124, 124)
+                .addGap(61, 61, 61)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(fecha)
                     .addComponent(inputFecha, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -135,7 +169,7 @@ public class comprasPorFecha extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(resultado, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4))
-                .addContainerGap(124, Short.MAX_VALUE))
+                .addContainerGap(131, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -144,7 +178,7 @@ public class comprasPorFecha extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 743, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -163,6 +197,17 @@ public class comprasPorFecha extends javax.swing.JFrame {
         String fecha = "'"+inputFecha.getText()+"'";
         resultado.setText(llamar(fecha).toString());
     }//GEN-LAST:event_botonActionPerformed
+
+    private void jBotonVolverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBotonVolverActionPerformed
+        AdGeneralInicio obj =new AdGeneralInicio();
+        obj.setVisible(true);
+        dispose();
+        try {
+            MySQL.conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(menu.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jBotonVolverActionPerformed
 
     /**
      * @param args the command line arguments
@@ -205,6 +250,8 @@ public class comprasPorFecha extends javax.swing.JFrame {
     private javax.swing.JButton boton;
     private javax.swing.JLabel fecha;
     private javax.swing.JTextField inputFecha;
+    private javax.swing.JButton jBotonVolver;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
